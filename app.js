ceb9f9967cf87aa4d83260688c64e3fcbb64f968
Prajapati omkar <prajapatiomkar01@gmail.com>
2022-09-07 18:38:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const app = express();
+app.set("view engine", "ejs");
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articalSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articalSchema);
+
+
+app.route("/articles")
+    .get(function (req, res) {
+        Article.find(function (err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles)
+            } else {
+                res.send(err)
+            }
+        })
+    })
+    .post(function (req, res) {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Successfully added a new article.");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send("Successfully deleted all articles.");
+            } else {
+                res.send(err);
+            }
+        })
+    });
+
+app.listen(3000, function () {
+    console.log("http://localhost:3000")
+});
