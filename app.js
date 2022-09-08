b2d81257300e277aa464c6546589778cef2ef254
Prajapati omkar <prajapatiomkar01@gmail.com>
2022-09-08 18:14:18 +0530

    Successfully completed REST implementation Project

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const e = require('express');
 
 const app = express();
 app.set("view engine", "ejs");
@@ -49,6 +50,55 @@ app.route("/articles")
         Article.deleteMany(function (err) {
             if (!err) {
                 res.send("Successfully deleted all articles.");
+            } else {
+                res.send(err);
+            }
+        })
+    });
+
+app.route("/articles/:articleTitle")
+    .get(function (req, res) {
+        Article.findOne({ title: req.params.articleTitle }, function (err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err)
+            }
+        })
+    })
+    .put(function (req, res) {
+        Article.updateOne(
+            { title: req.params.articleTitle },
+            {
+                title: req.body.title,
+                content: req.body.content
+            },
+
+            function (err) {
+                if (!err) {
+                    res.send("Successfully Updated articles.")
+                }
+            }
+        )
+    })
+    .patch(function (req, res) {
+        Article.updateOne(
+            { title: req.params.articleTitle },
+            { $set: req.body },
+            function (err) {
+                if (!err) {
+                    res.send("Succesfully updated article")
+                } else {
+                    res.send(err);
+                }
+            }
+
+        )
+    })  
+    .delete(function (req, res) {
+        Article.deleteOne({ title: req.params.articleTitle }, function (err) {
+            if (!err) {
+                res.send("Deleted Successfully");
             } else {
                 res.send(err);
             }
